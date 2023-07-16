3de12acfbc25cee38d5a9bc5d5867064f2fadc94
unknown <marsandnoa@gmail.com>
2023-07-15 21:54:11 -0400

    need to implement what to do what target item is found

--- webScraper.py
+++ webScraper.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import requests
-
+import time
 app = Flask(__name__)
 
 
@@ -10,76 +10,84 @@ def callback():
     # Handle the authorization code received from the OAuth provider
     authorization_code = request.args.get('code')
 
-    # Make a POST request to the token endpoint to exchange the authorization code for an access token
-client_id = 'slisealtradescraper'
-client_secret = 'zFq9C5h0RNMA'
-grant_type = 'client_credentials'
-scope = 'service:psapi'
+while(True):
+    client_id = 'slisealtradescraper'
+    client_secret = ''
+    grant_type = 'client_credentials'
+    scope = 'service:psapi'
+    version = '1.0.0'
+    contact = 'marsandnoa@gmail.com'
+    # Request payload
 
-# Request payload
-payload = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'grant_type': grant_type,
-    'scope': scope
-}
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': grant_type,
+        'scope': scope
+    }
 
-client_id = 'slisealtradescraper'
-version = '1.0.0'
-contact = 'marsandnoa@gmail.com'
+    headers = {
+        'User-Agent': f'OAuth {client_id}/{version} (contact: {contact}) StrictMode'
+    }
 
-headers = {
-    'User-Agent': f'OAuth {client_id}/{version} (contact: {contact}) StrictMode'
-}
+    response = requests.post('https://www.pathofexile.com/oauth/token', data=payload,headers=headers)
 
-response = requests.post('https://www.pathofexile.com/oauth/token', data=payload,headers=headers)
+    if response.status_code == 200:
+        # Access token obtained successfully
+        response_data = response.json()
+        access_token = response_data['access_token']
+        expires_in = response_data['expires_in']
+        token_type = response_data['token_type']
+        username = response_data['username']
+        sub = response_data['sub']
+        scope = response_data['scope']
+    else:
+        print("error on access fetch")
 
-# Access the response headers
-headers = response.headers
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'User-Agent': f'OAuth {client_id}/{version} (contact: {contact}) StrictMode'
+    }
 
-# Print the response headers
-print(response.status_code)
-for header, value in headers.items():
-    print(header + ": " + value)
+    base_url = 'https://api.pathofexile.com'
+    endpoint = '/public-stash-tabs'
+    url = f"{base_url}{endpoint}"
+    response = requests.get(url, headers=headers)
 
-if response.status_code == 200:
-    # Access token obtained successfully
-    response_data = response.json()
-    access_token = response_data['access_token']
-    expires_in = response_data['expires_in']
-    token_type = response_data['token_type']
-    username = response_data['username']
-    sub = response_data['sub']
-    scope = response_data['scope']
 
-    # Handle the obtained access token as needed
-    # Store it securely, use it to make authenticated API requests, etc.
-    print(f'Access Token: {access_token}')
-else:
-    # Handle token request failure
-    print('Token request failed')
+    #target item format, see API for list of item keys
+    TargetItem={'name':"Chin Sol"}
 
-headers = {
-    'Authorization': f'Bearer {access_token}',
-    'User-Agent': f'OAuth {client_id}/{version} (contact: {contact}) StrictMode'
-}
+    # Check the response status code
+    if response.status_code == 200:
+        data = response.json()
+        next_change_id = data['next_change_id']  # Pagination code for the next request
 
-base_url = 'https://api.pathofexile.com'
-endpoint = '/public-stash-tabs'
+        while next_change_id:
+            stashes = data['stashes']
+            for stash in stashes:
+                stash_id = stash['id']
+                public = stash['public']
+                for item in stash['items']:
+                    isidentical=True
+                    for key in TargetItem.keys():
+                        if(TargetItem[key]):
+                            if TargetItem[key] != item[key]:
+                                isidentical=False;
+                    if(isidentical):
+                        #need to implement stuff when target item found
+                        print(item)
 
-# Construct the URL
-url = f"{base_url}{endpoint}"
+            next_url = f"{base_url}{endpoint}/{next_change_id}"
+            next_response = requests.get(next_url, headers=headers)
+            time.sleep(.5)
+            if(next_response.status_code!=403):
+                data = next_response.json()
+                if(data['next_change_id']):
+                    next_change_id = data['next_change_id']
 
-# Send the GET request
-response = requests.get(url, headers=headers)
-
-# Check the response status code
-if response.status_code == 200:
-    # Request successful, handle the response data
-    data = response.json()
-    # Process the data as needed
-else:
-    # Request failed, handle the error
-    print(f"Request failed with status code: {response.status_code}")
-
-print(data)
+    # End of the stream
+        print("Reached the end of the stream.")
+    else:
+        print("error on item fetch")
+    time.sleep(30)
